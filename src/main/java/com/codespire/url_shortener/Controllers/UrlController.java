66678a002867b185cef8b55dc429ed0e2f9deec7
Siddu Mokhasi <128201797+devsiddu@users.noreply.github.com>
2026-02-06 14:36:25 +0530

    completed the backend (#2)
    * completed the backend
    
    * Add validation to URL request and implement global exception handling
    
    * Enhance URL validation and add global exception handling for invalid short codes

--- src/main/java/com/codespire/url_shortener/Controllers/UrlController.java
+++ src/main/java/com/codespire/url_shortener/Controllers/UrlController.java
@@ -1,8 +1,53 @@
 package com.codespire.url_shortener.Controllers;
 
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codespire.url_shortener.Dto.UrlRequest;
+import com.codespire.url_shortener.Dto.UrlResponse;
+import com.codespire.url_shortener.Models.ShortUrl;
+import com.codespire.url_shortener.Services.UrlService;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 @RestController
+@Validated
 public class UrlController {
-    
+
+    private UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<UrlResponse> generate(@Valid @RequestBody UrlRequest request) {
+        String originalUrl = request.getOriginalUrl();
+
+        ShortUrl shortUrl = urlService.createOrGetShortUrl(originalUrl);
+        String fullResponse = "http://localhost:8080/" + shortUrl.getShortCode();
+        return ResponseEntity.ok(new UrlResponse(fullResponse));
+
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@NotBlank(message = "Short code must not be empty") @Pattern(regexp = "^[a-zA-Z0-9]{6}$", message = "Invalid short code format") @PathVariable("shortCode") String shortCode) {
+
+        ShortUrl shortUrl = urlService.getByShortCode(shortCode);
+        if (shortUrl == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(shortUrl.getOriginalUrl())).build();
+    }
 }
---
+++ src/main/java/com/codespire/url_shortener/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.codespire.url_shortener.Exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handelErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), err.getDefaultMessage());
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handelPramsError(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", "Invalid short code format");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
